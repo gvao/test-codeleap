4fa822191ee160ed3e33570856dfd139e94c39ca
Yuri Galvao <yuri.gvao@gmail.com>
2023-06-13 14:09:54 -0300

    Created common button

---
+++ src/common/button/index.tsx
@@ -0,0 +1,22 @@
+import styles from "./styles.module.css"
+
+type ButtonProps = {
+    isValid?: boolean,
+    // type?: string,
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const Button = ({ children, type, isValid = false, ...props }: ButtonProps) => (
+    <div
+        className={styles.wrap}
+    >
+
+        <button
+            className={`${styles.button} ${!isValid && styles.invalid}`}
+            {...props}
+            type={isValid ? type : 'button'}
+        >
+            {children}
+        </button>
+
+    </div>
+)
--- src/components/signup/index.tsx
+++ src/components/signup/index.tsx
@@ -1,6 +1,7 @@
-import { Input } from "@/common/input";
 import styles from "./styles.module.css"
 
+import { Input } from "@/common/input";
+import { Button } from "@/common/button";
 
 export const Signup = () => (
     <div className={styles.wrap} >
@@ -9,7 +10,7 @@ export const Signup = () => (
   
       <form>
         <Input label="Please enter your username" />
-        <button type="submit">enter</button>
+        <Button type="submit">enter</Button>
       </form>
   
     </div>
