5a3c806c986b2deec6ebe398b13a96d420319a82
Yuri Galvao <yuri.gvao@gmail.com>
2023-06-15 11:35:17 -0300

    Create app routes

---
+++ src/constantes/routes-app.tsx
@@ -0,0 +1,13 @@
+const ROUTES_APP = {
+    public: {
+        signup: '/profile/signup',
+        signin: '/profile/signin',
+    },
+    private: {
+        logout: '/profile/logout',
+        dashboard: '/',
+    }
+
+}
+
+export default ROUTES_APP
