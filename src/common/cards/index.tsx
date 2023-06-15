fbe73927ab9534b64abc03ab9e1179e30dfe4645
Yuri Galvao <yuri.gvao@gmail.com>
2023-06-15 11:36:44 -0300

    updated card component

--- src/common/cards/index.tsx
+++ src/common/cards/index.tsx
@@ -1,4 +1,7 @@
+import { useAuthContext } from "@/context/auth"
 import styles from "./styles.module.css"
+import { ButtonAction } from "@/common/button"
+import { MouseEventHandler } from "react"
 
 type CardPostProps = {
     title: string,
@@ -7,22 +10,41 @@ type CardPostProps = {
     created_datetime: string,
 }
 
-export const CardPost = ({ title, content, username, created_datetime }: CardPostProps) => {
- 
+export const CardPost = ({ title, content, username: usernameProp, created_datetime }: CardPostProps) => {
+
+    const {
+        username,
+    } = useAuthContext()
+
+    const actionConfirme: MouseEventHandler<HTMLButtonElement> = ({ target }) => {
+        console.log(target.alt)
+    }
+
     return (
         <article className={styles.card}>
             <header className={styles.header}>
                 <h2>{title}</h2>
 
-                <div className={styles.action}>
-                    <img src="/edit.png" alt="edit post" />
-                    <img src="/delete-forever.png" alt="delete post" />
-                </div>
+                {username === usernameProp && (
+                    <div className={styles.action}>
+                        <ButtonAction
+                            onClick={actionConfirme}
+                        >
+                            <img src="/edit.png" alt="edit post" />
+                        </ButtonAction>
+
+                        <ButtonAction
+                            onClick={actionConfirme}
+                        >
+                            <img src="/delete-forever.png" alt="delete post" />
+                        </ButtonAction>
+                    </div>
+                )}
             </header>
 
             <div className={styles.container}>
                 <header className={styles["infos-post"]}>
-                    <span className={styles["profile-name"]}>{`@${username}`}</span>
+                    <span className={styles["profile-name"]}>{`@${usernameProp}`}</span>
                     <span>{created_datetime}</span>
                 </header>
 
